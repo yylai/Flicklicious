7fdf0935e3c4aacd876ec2a99b1f3d94b7337501
Yin Lai <yylai.git@gmail.com>
2016-10-16 02:45:25 -0700

    added refresh

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
@@ -26,6 +27,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(self.refreshControlAction(refreshControl:)), for: .valueChanged)
+        movieTableView.insertSubview(refreshControl, at: 0)
+        
         movieTableView.dataSource = self
         movieTableView.delegate = self
         
@@ -37,12 +43,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue:OperationQueue.main
         )
         
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
         let task : URLSessionDataTask = session.dataTask(with: request,completionHandler: { (dataOrNil, response, error) in
             if let data = dataOrNil {
                 if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
                     NSLog("response: \(responseDictionary)")
                     self.movies = responseDictionary["results"] as! [NSDictionary]
                     self.movieTableView.reloadData()
+                    MBProgressHUD.hide(for: self.view, animated: true)
                 }
             }
         });
@@ -89,6 +98,29 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        let url = URL(string:"\(rootUrl)\(endPoint)?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+
+        
+        let task : URLSessionDataTask = session.dataTask(with: request,completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
+                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                    self.movieTableView.reloadData()
+                    refreshControl.endRefreshing()
+                }
+            }
+        });
+        task.resume()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UITableViewCell
         let indexPath = movieTableView.indexPath(for: cell)
@@ -100,6 +132,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         
     }
+    
+//    private let params = ["api-key": "53eb9541b4374660d6f3c0001d6249ca:19:70900879"]
+//    private let resourceUrl = "http://api.nytimes.com/svc/topstories/v1/home.json"
+//    class func fetchStories(successCallback: ([Story]) -> Void, error: ((NSError?) -> Void)?) {
+//        let manager = AFHTTPRequestOperationManager()
+//        manager.GET(resourceUrl, parameters: params, success: { (operation ,responseObject) -> Void in
+//            if let results = responseObject["results"] as? NSArray {
+//                var stories: [Story] = []
+//                for result in results as [NSDictionary] {
+//                    stories.append(Story(jsonResult: result))
+//                }
+//                successCallback(stories)
+//            }
+//            }, failure: { (operation, requestError) -> Void in
+//                if let errorCallback = error? {
+//                    errorCallback(requestError)
+//                }
+//        })
+//    }
 
 
 }
